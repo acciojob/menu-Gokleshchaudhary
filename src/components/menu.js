03875c1c6fcb9861f516d9e773da09d1820d4640
goklesh chaudhary <123gopalchaudhary123@gmail.com>
2025-08-01 12:09:03 +0530

    Update menu.js

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,1 +1,28 @@
+import React from "react";
+
+function Menu({ items }) {
+  return (
+    <div className="section-center">
+      {items.map((item) => {
+        const { id, title, img, desc, price, category } = item;
+        const testId = `menu-item-${category}`;
+
+        return (
+          <article key={id} className="menu-item" data-test-id={testId}>
+            <img src={img} alt={title} className="photo" />
+            <div className="item-info">
+              <header>
+                <h4>{title}</h4>
+                <h4 className="price">{price}</h4>
+              </header>
+              <p className="item-text">{desc}</p>
+            </div>
+          </article>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Menu;
 
