ca52db6c04b661b19a7bda100068eef1a9826978
goklesh chaudhary <123gopalchaudhary123@gmail.com>
2025-08-01 12:08:04 +0530

    Update data.js

--- src/components/data.js
+++ src/components/data.js
@@ -1,1 +1,53 @@
+const menu = [
+  {
+    id: 1,
+    title: "Buttermilk Pancakes",
+    category: "breakfast",
+    price: "$15.99",
+    img: "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-1.jpeg",
+    desc: "I'm baby woke mlkshk wolf bitters live-edge blue bottle."
+  },
+  {
+    id: 2,
+    title: "Diner Double",
+    category: "lunch",
+    price: "$11.99",
+    img: "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-2.jpeg",
+    desc: "Vaporware iPhone mumblecore selvage raw denim slow-carb."
+  },
+  {
+    id: 3,
+    title: "Godzilla Milkshake",
+    category: "shakes",
+    price: "$6.99",
+    img: "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-3.jpeg",
+    desc: "Ombucha chillwave fanny pack 3 wolf moon street art."
+  },
+  {
+    id: 4,
+    title: "Country Delight",
+    category: "breakfast",
+    price: "$20.99",
+    img: "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-4.jpeg",
+    desc: "Shabby chic keffiyeh neutra snackwave pork belly."
+  },
+  {
+    id: 5,
+    title: "Egg Attack",
+    category: "lunch",
+    price: "$22.99",
+    img: "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-5.jpeg",
+    desc: "Franzen vegan pabst bicycle rights kickstarter."
+  },
+  {
+    id: 6,
+    title: "Oreo Dream",
+    category: "shakes",
+    price: "$18.99",
+    img: "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-6.jpeg",
+    desc: "Portland chicharrones ethical edison bulb."
+  },
+];
+
+export default menu;
 
