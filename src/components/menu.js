c8262223ce6a6af202f0490cce0e3496882d0caa
goklesh chaudhary <123gopalchaudhary123@gmail.com>
2025-08-04 10:53:48 +0530

    Update menu.js

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,19 +1,18 @@
+// src/components/Menu.js
 import React from 'react';
 
 const Menu = ({ items }) => {
   return (
-    <div className="menu-container">
+    <div className="menu">
       {items.map(item => (
         <div
           key={item.id}
           className="menu-item"
           data-test-id={`menu-item-${item.category}`}
         >
+          <h3>{item.name}</h3>
           <img src={item.image} alt={item.name} />
-          <div className="menu-details">
-            <h3>{item.name}</h3>
-            <p>{item.price}</p>
-          </div>
+          <p>{item.price}</p>
         </div>
       ))}
     </div>
