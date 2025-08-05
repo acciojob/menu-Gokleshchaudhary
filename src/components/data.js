bfbd0477a0ccefcb7e5ded49905ed01053894b89
goklesh chaudhary <123gopalchaudhary123@gmail.com>
2025-08-05 11:24:27 +0530

    Update data.js

--- src/components/data.js
+++ src/components/data.js
@@ -1,47 +1,25 @@
 const menu = [
   {
     id: 1,
-    name: 'Pancakes',
-    category: 'Breakfast',
-    image: 'https://via.placeholder.com/150',
-    price: '$5.99',
+    title: "Pancakes",
+    category: "breakfast",
+    price: 5.99,
+    img: "https://via.placeholder.com/150",
   },
   {
     id: 2,
-    name: 'Burger',
-    category: 'Lunch',
-    image: 'https://via.placeholder.com/150',
-    price: '$8.99',
+    title: "Burger",
+    category: "lunch",
+    price: 8.99,
+    img: "https://via.placeholder.com/150",
   },
   {
     id: 3,
-    name: 'Milkshake',
-    category: 'Shakes',
-    image: 'https://via.placeholder.com/150',
-    price: '$3.99',
-  },
-  {
-    id: 4,
-    name: 'Eggs & Toast',
-    category: 'Breakfast',
-    image: 'https://via.placeholder.com/150',
-    price: '$4.50',
-  },
-  {
-    id: 5,
-    name: 'Chicken Sandwich',
-    category: 'Lunch',
-    image: 'https://via.placeholder.com/150',
-    price: '$7.50',
-  },
-  {
-    id: 6,
-    name: 'Strawberry Shake',
-    category: 'Shakes',
-    image: 'https://via.placeholder.com/150',
-    price: '$4.25',
+    title: "Milkshake",
+    category: "shakes",
+    price: 4.99,
+    img: "https://via.placeholder.com/150",
   },
 ];
 
 export default menu;
-
