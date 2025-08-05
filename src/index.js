1f22d72e9c1ee1e016725ed92b9b9d44c9b62ce4
goklesh chaudhary <123gopalchaudhary123@gmail.com>
2025-08-05 11:13:51 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,4 +3,3 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App'; // ✅ if App.js is in /components
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
