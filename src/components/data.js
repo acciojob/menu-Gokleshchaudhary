3d1764e642f6b55809ae5b402e51ad205c1944cb
goklesh chaudhary <123gopalchaudhary123@gmail.com>
2025-08-05 11:16:21 +0530

    Update data.js

--- src/components/data.js
+++ src/components/data.js
@@ -1,1 +1,47 @@
+const menu = [
+  {
+    id: 1,
+    name: 'Pancakes',
+    category: 'Breakfast',
+    image: 'https://via.placeholder.com/150',
+    price: '$5.99',
+  },
+  {
+    id: 2,
+    name: 'Burger',
+    category: 'Lunch',
+    image: 'https://via.placeholder.com/150',
+    price: '$8.99',
+  },
+  {
+    id: 3,
+    name: 'Milkshake',
+    category: 'Shakes',
+    image: 'https://via.placeholder.com/150',
+    price: '$3.99',
+  },
+  {
+    id: 4,
+    name: 'Eggs & Toast',
+    category: 'Breakfast',
+    image: 'https://via.placeholder.com/150',
+    price: '$4.50',
+  },
+  {
+    id: 5,
+    name: 'Chicken Sandwich',
+    category: 'Lunch',
+    image: 'https://via.placeholder.com/150',
+    price: '$7.50',
+  },
+  {
+    id: 6,
+    name: 'Strawberry Shake',
+    category: 'Shakes',
+    image: 'https://via.placeholder.com/150',
+    price: '$4.25',
+  },
+];
+
+export default menu;
 
